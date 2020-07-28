17b2ac7eed0f314c6a795d0c2ca626dfd639cf4f
analista5 <analista5.dgi@gmail.com>
2020-07-27 22:26:49 -0500

    prueba

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import environ
 import os
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
