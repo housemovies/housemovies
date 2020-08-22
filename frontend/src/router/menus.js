b763b482c93195d09274c58fcd4cef4d54497486
analista5 <analista5.dgi@gmail.com>
2020-08-21 19:42:59 -0500

    cambios

--- frontend/src/router/menus.js
+++ frontend/src/router/menus.js
@@ -2,6 +2,7 @@
 const routes = [
   { path: '/principal/categoria/:id/:categoria', name: 'MenusAdministrar',  props: true, component: () => import('pages/menu/index.vue') },
   { path: '/principal/pelicula/:id', name: 'PeliculasDetalle',  props: true, component: () => import('pages/menu/pelicula.vue') },
+  { path: '/principal/pelicula/detallado/:id', name: 'Detallado',  props: true, component: () => import('pages/menu/peliculaaleatoria.vue') },
   // { path: '/solicitud/solicitudes/crear/:id', name: 'SolicitudesCrearBasico', props: true, component: () => import('pages/solicitud/partial/crear.vue') },
   // { path: '/solicitud/solicitudes/crear', name: 'SolicitudesCrearBasico', component: () => import('pages/solicitud/partial/crear.vue') },
   // { path: '/solicitud/solicitudes/editar/:id', name: 'SolicitudesEditar', props: true, component: () => import('pages/inmueble/partial/editar.vue') }
